a1389b5c1738d9f753534723cc1f2d843c081462
Steve Fitzgerald <steve@csun.edu>
2021-11-16 16:24:43 -0800

    fixed typo in comment

--- mips/examples/hello_world.asm
+++ mips/examples/hello_world.asm
@@ -1,14 +1,13 @@
 	.data
-str:	.asciiz "Hello World\n" # H,e,l,l,o, ,W,o,r,l,d,\,\0
+str:	.asciiz "Hello World\n" # H,e,l,l,o, ,W,o,r,l,d,\n,\0
 
 	.text
 	.globl main
 
-main:	nop
+main:   nop
 	li $v0, 4	# v0 = 4 (print_string == 4)
 	la $a0, str 	# a0 = & str
 	syscall		# trap: print_string(str)
-
 
 	li $v0, 10   	# v0 = 10 (terminate == 10)
 	syscall   	# trap: terminate
